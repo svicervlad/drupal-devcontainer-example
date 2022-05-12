05bf12ff6e532110260e8f3b18816aad52d7fe32
svicervlad <vlad.sadretdinov@jet.dev>
2022-05-12 07:28:19 +0000

    feat: implement basic service

---
+++ web/modules/lucidpress_dam/src/Collection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\lucidpress_dam;
+
+/**
+ * Service description.
+ */
+class Collection {
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    // @DCG place your code here.
+  }
+
+}
