4c5523092cdf6e90c362d6863451f33c182a6955
Vlad <vlad.sadretdinov@jet.dev>
2022-12-17 12:06:05 +0000

    fix: script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -10,18 +10,12 @@ normal=$(tput sgr0)
 if [ ! -L "/code" ]; then
   sudo ln -sf $(pwd) /code
 fi
-# get confirmation
-read -N 1 -p "This will drop database and rebuild site. Continue (y/n)? " answer
-echo
-
-if [ "${answer,,}" == "n" ]
-then
-    printf "\n${SUCCESS_COLOR}${bold}Stop rebuild site...\n${NORMAL_COLOR}${normal}"
-    exit 0;
-fi
 
 printf "\n${SUCCESS_COLOR}${bold}Run composer install...\n\n${NORMAL_COLOR}";
 composer install -n || { printf "${ERROR_COLOR}${bold}An error occurred while install dependencies by composer.\n${NORMAL_COLOR}${normal}"; exit 1; };
+if [ ! -L "/usr/bin/local/drush" ]; then
+  sudo ln -sf ./vendor/bin/drush /usr/bin/local/drush
+fi
 
 printf "\n${SUCCESS_COLOR}${bold}Install standart drupal...\n\n${NORMAL_COLOR}${normal}";
 drush si --site-name=DEV --account-pass=admin -y
